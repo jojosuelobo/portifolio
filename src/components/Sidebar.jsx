800a3a6edda2824c0d8df7d23de6ca1086f46bd0
jojosuelobo <josuelobo2000@gmail.com>
2023-11-28 17:31:49 -0300

    first commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,25 @@
+import SocialNetworkContainer from "./SocialNetworkContainer";
+import InformationContainer from "./InformationContainer";
+
+import Avatar from "../img/eu.jpg";
+
+import "../styles/components/sidebar.sass";
+
+const Sidebar = () => {
+  return (
+    <aside id="sidebar">
+      <img src={Avatar} alt="Matheus Battisti" />
+      <div className="tags">
+        <p className="title">Desenvolvedor Web</p>
+        <p className="subtitle">Analista de Testes QA</p>
+      </div>
+      <SocialNetworkContainer />
+      <InformationContainer />
+      <a href="#" className="btn">
+        Download currículo
+      </a>
+    </aside>
+  );
+};
+
+export default Sidebar;
