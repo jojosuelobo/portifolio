7b699c1ec92456735f8e379124939885a3ccd452
jojosuelobo <josuelobo2000@gmail.com>
2023-11-28 21:51:50 -0300

    atualização aba sobre

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -1,18 +1,22 @@
+import "../styles/components/maincontent.sass";
+
 const AboutContainer = () => {
   return (
     <section className="about-container">
       <h2>Sobre</h2>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod, dolores!
-        Repellendus eius consequuntur, voluptate deserunt perferendis mollitia
-        debitis est autem aperiam necessitatibus beatae assumenda illo rem
-        architecto quis dolor quae.
+        Apaixonado por tecnologia, bacharel em ciência da computação na <a className="bold">Universidade Vila Velha (UVV)</a>.
+        Atuo como  <a className="bold">Analista de Qualidade de Software (QA)</a> com experiência em <a className="bold">testes manuais e automatizados</a>.
+        Proficiente em levantamento de requisitos, elaboração de testes e execução de casos de teste.
       </p>
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos veritatis
-        aperiam, odit nihil placeat, dolorum sequi reprehenderit fugiat,
-        distinctio provident impedit modi neque sapiente voluptate voluptates.
-        Maiores sint at dolores?
+        Conhecimento em planejamento de testes, cenários de testes , casos de testes, pirâmide de testes, testes funcionais, testes não funcionais,
+        testes exploratórios e testes de regressão, automação de testes da Web com <a className="bold">Ghost Inspector</a>, <a className="bold">Selenium</a> e <a className="bold">Cypress</a>, consulta em banco de dados com SQL
+        conhecimento de Front-end e Back-end
+      </p>
+      <p>
+        Dedicado, empático, determinado, autodidata, tenho muita força de vontade, tenho facilidade de aprender, trabalho bem em equipe
+
       </p>
     </section>
   );
