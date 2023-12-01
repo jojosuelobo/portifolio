e13fb53d0c12326f489df9ac47386cf1c4b8ebd9
josue <josue@octuspay.com>
2023-12-01 15:36:09 -0300

    ajusts in projects

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,3 +1,5 @@
+import "../styles/components/ProjectsContainer.sass"
+
 import { useTranslation } from "react-i18next";
 
 const ProjectsContainer = () => {
@@ -6,12 +8,6 @@ const ProjectsContainer = () => {
   return (
     <section className="projects-container">
       <h2>{t("labels.projetos")}</h2>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore at
-        rem fugit provident dolorum exercitationem, ducimus quisquam ratione
-        cumque reiciendis accusamus totam, veniam iusto numquam distinctio.
-        Dignissimos labore aspernatur pariatur?
-      </p>
       <a target="_blank" href="https://github.com/jojosuelobo?tab=repositories" className="btn">
       {t("labels.botaoProjetos")}
       </a>
